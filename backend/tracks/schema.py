d17608307d76e35c47c252f33b7a51405e2e5b75
MaksimLion <maxim226356@mail.ru>
2019-05-13 15:03:22 +0300

    base features

---
+++ backend/tracks/schema.py
@@ -0,0 +1,19 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Track
+
+
+class TrackType(DjangoObjectType):
+
+    class Meta:
+        model = Track
+
+
+class Query(graphene.ObjectType):
+    tracks = graphene.List(TrackType)
+
+    def resolve_tracks(self, info):
+        return Track.objects.all()
+
+
+class CreateTrack(graphene.Mutation)
